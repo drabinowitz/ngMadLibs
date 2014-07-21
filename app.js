955722851b662020ed6757e411005573d7ac54ad
Morgan Polotan <morgan.polotan@tapad.com>
2014-07-21 19:34:16 -0400

    formatting

--- app.js
+++ app.js
@@ -1,31 +1,59 @@
 angular.module('madlibs', [])
-
 	.controller('myController',['$scope', function($scope) {
 	
-		$scope.madlibs={
-	femaleName:{word:'Female name',index:1},
-	jobTitle:{word:'Job title',index:2},
-	tediousTask:{word:'Tedious task',index:3},
-	dirtyTask:{word:'Dirty task',index:4},
-	celebrity:{word:'Celebrity',index:5},
-	uselessSkill:{word:'Useless skill',index:6},
-	obnoxiousCelebrity:{word:'Obnoxious celebrity',index:7},
-	hugeNumber:{word:'Huge number',index:8},
-	adjective:{word:'Adjective',index:9}
-}
+		$scope.madlibs = {
+			femaleName: {
+				word: 'Female name',
+				index: 1
+			},
+			jobTitle: {
+				word: 'Job title',
+				index: 2
+			},
+			tediousTask: {
+				word: 'Tedious task',
+				index: 3
+			},
+			dirtyTask: { 
+				word: 'Dirty task',
+				index: 4
+			},
+			celebrity: { 
+				word: 'Celebrity', 
+				index: 5
+			},
+			uselessSkill: {
+				word: 'Useless skill',
+				index: 6
+			},
+			obnoxiousCelebrity: {
+				word: 'Obnoxious celebrity',
+				index: 7
+			},
+			hugeNumber: {
+				word: 'Huge number',
+				index: 8
+			},
+			adjective: {
+				word: 'Adjective',
+				index: 9
+			}
+		};
 
-	}])
+	}]).filter('orderObjectBy', function() {
+  		return function(items, field, reverse) {
+    		var filtered = [];
 
-	.filter('orderObjectBy', function() {
-  return function(items, field, reverse) {
-    var filtered = [];
-    angular.forEach(items, function(item) {
-      filtered.push(item);
-    });
-    filtered.sort(function (a, b) {
-      return (a[field] > b[field] ? 1 : -1);
-    });
-    if(reverse) filtered.reverse();
-    return filtered;
-  };
-});
+		    angular.forEach(items, function(item) {
+		      filtered.push(item);
+		    });
+
+		    filtered.sort(function (a, b) {
+		      return (a[field] > b[field] ? 1 : -1);
+		    });
+
+		    if(reverse) filtered.reverse();
+
+		    return filtered;
+	  };
+	});
