46636a37c1c082f7c68461720eb18dcebbb180fe
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-07-22 11:48:53 -0400

    resolved after merge conflicts

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 angular.module('madlibs', [])
-<<<<<<< HEAD
 
 	.controller('mlInputCtrl', function($rootScope,$scope) {
 
@@ -143,64 +142,5 @@ angular.module('madlibs', [])
 	    return genderInput == "male"? pronouns[input][0] : pronouns[input][1];
 	
 	  };
-	
-=======
-	.controller('myController',['$scope', function($scope) {
-	
-		$scope.madlibs = {
-			femaleName: {
-				word: 'Female name',
-				index: 1
-			},
-			jobTitle: {
-				word: 'Job title',
-				index: 2
-			},
-			tediousTask: {
-				word: 'Tedious task',
-				index: 3
-			},
-			dirtyTask: { 
-				word: 'Dirty task',
-				index: 4
-			},
-			celebrity: { 
-				word: 'Celebrity', 
-				index: 5
-			},
-			uselessSkill: {
-				word: 'Useless skill',
-				index: 6
-			},
-			obnoxiousCelebrity: {
-				word: 'Obnoxious celebrity',
-				index: 7
-			},
-			hugeNumber: {
-				word: 'Huge number',
-				index: 8
-			},
-			adjective: {
-				word: 'Adjective',
-				index: 9
-			}
-		};
 
-	}]).filter('orderObjectBy', function() {
-  		return function(items, field, reverse) {
-    		var filtered = [];
-
-		    angular.forEach(items, function(item) {
-		      filtered.push(item);
-		    });
-
-		    filtered.sort(function (a, b) {
-		      return (a[field] > b[field] ? 1 : -1);
-		    });
-
-		    if(reverse) filtered.reverse();
-
-		    return filtered;
-	  };
->>>>>>> master
 	});
